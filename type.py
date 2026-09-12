bd290187fbf2ba3744225a99325489d5e54d7b92
Sanchita-Hasure21 <sanchitahasure@gmail.com>
2026-09-12 22:23:18 +0530

    Delete type.py

--- type.py
+++
@@ -1,9 +0,0 @@
-name = input("Enter your name: ")
-age = int(input("Enter your age: "))
-marks = float(input("Enter your marks: "))
-passed = True
-
-print(name, type(name))
-print(age, type(age))
-print(marks, type(marks))
-print(passed, type(passed))
